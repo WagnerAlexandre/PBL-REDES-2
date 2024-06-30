08345b520a6f1eed65a40058f1f9fe86376d2714
Wagner Alexandre <68972860+WagnerAlexandre@users.noreply.github.com>
2024-06-29 21:31:56 -0300

    :wrench: fix: registro via requisição com json

--- banco-brasileirinho/main.go
+++ banco-brasileirinho/main.go
@@ -54,11 +54,9 @@ type CadastroPFPJ struct {
 }
 
 type CadastroCJ struct {
-	CPF1     string `json:"cpf1"`
-	CPF2     string `json:"cpf2"`
-	NumConta int    `json:"numconta"`
-	Tipo     int    `json:"tipo"`
-	Senha    string `json:"senha"`
+	CPF1  string `json:"cpf1"`
+	CPF2  string `json:"cpf2"`
+	Senha string `json:"senha"`
 }
 
 // mutex para numero de conta
@@ -83,11 +81,23 @@ var IndexcontasPJ = make(map[string][]*ContaPJ)
 var IndexcontasCJ = make(map[string][]*ContaCJ)
 
 func rota_Cadastrar_PF(c *gin.Context) {
-	var conta ContaPF
-	if err := c.BindJSON(&conta); err != nil {
+	var cadastro CadastroPFPJ
+	if err := c.BindJSON(&cadastro); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+
+	if checaExistencia(cadastro.CPFCNPJ) {
+		c.JSON(http.StatusConflict, gin.H{"error": "Você já possui uma conta com este CPF"})
+		return
+	}
+
+	var conta ContaPF
+	conta.CPF = cadastro.CPFCNPJ
+	conta.Nome = cadastro.Nome
+	conta.Senha = cadastro.Senha
+	conta.Tipo = 1 // Tipo para Pessoa Física
+
 	criar_conta_pf(&conta)
 	c.JSON(http.StatusCreated, gin.H{"message": "Conta criada com sucesso", "conta": conta})
 }
@@ -110,42 +120,44 @@ func rota_Cadastrar_PJ(c *gin.Context) {
 		return
 	}
 
-	var novaConta *ContaPJ
-	novaConta = criar_conta_pj(&cadastro)
+	if checaExistencia(cadastro.CPFCNPJ) {
+		c.JSON(http.StatusConflict, gin.H{"error": "Você já possui uma conta com este CNPJ"})
+		return
+	}
+
+	novaConta := criar_conta_pj(&cadastro)
 	c.JSON(http.StatusCreated, novaConta)
 }
 
-func criar_conta_pj(novaConta *CadastroPFPJ) *ContaPJ {
-	var conta ContaPJ
-	conta.Nome = novaConta.Nome
-	conta.CNPJ = novaConta.CPFCNPJ
-	conta.Senha = novaConta.Senha
-
-	conta.NumConta = criar_NumConta()
-	conta.Balanco = 0.0
+func criar_conta_pj(cadastro *CadastroPFPJ) *ContaPJ {
+	conta := &ContaPJ{
+		NumConta: criar_NumConta(),
+		CNPJ:     cadastro.CPFCNPJ,
+		Nome:     cadastro.Nome,
+		Senha:    cadastro.Senha,
+		Tipo:     2, // Tipo para Pessoa Jurídica
+		Balanco:  0.0,
+	}
 
 	// Armazena as informações da conta em TContasPJ
-	TContasPJ[conta.NumConta] = &conta
+	TContasPJ[conta.NumConta] = conta
 
-	// Indexa a conta usando o CPF
-	IndexcontasPJ[conta.CNPJ] = append(IndexcontasPJ[conta.CNPJ], &conta)
-	return &conta
+	// Indexa a conta usando o CNPJ
+	IndexcontasPJ[conta.CNPJ] = append(IndexcontasPJ[conta.CNPJ], conta)
+
+	return conta
 }
 
-func rota_Cadastrar_CJ(c *gin.Context) {
-	var conta ContaCJ
-	if err := c.BindJSON(&conta); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
+func criar_conta_cj(cadastro *CadastroCJ) *ContaCJ {
+	conta := &ContaCJ{
+		NumConta: criar_NumConta(),
+		CPF1:     cadastro.CPF1,
+		CPF2:     cadastro.CPF2,
+		Nome:     cadastro.CPF1 + " e " + cadastro.CPF2, // Opcional
+		Senha:    cadastro.Senha,
+		Tipo:     3, // Tipo para Conta Conjunta
+		Balanco:  0.0,
 	}
-	criar_conta_cj(&conta)
-
-	c.JSON(http.StatusCreated, gin.H{"message": "Conta conjunta criada com sucesso", "conta": conta})
-}
-
-func criar_conta_cj(conta *ContaCJ) {
-	conta.NumConta = criar_NumConta()
-	conta.Balanco = 0.0
 
 	// Armazena as informações da conta em TContasCJ
 	TContasCJ[conta.NumConta] = conta
@@ -153,6 +165,29 @@ func criar_conta_cj(conta *ContaCJ) {
 	// Indexa a conta usando os CPFs
 	IndexcontasCJ[conta.CPF1] = append(IndexcontasCJ[conta.CPF1], conta)
 	IndexcontasCJ[conta.CPF2] = append(IndexcontasCJ[conta.CPF2], conta)
+
+	return conta
+}
+
+func rota_Cadastrar_CJ(c *gin.Context) {
+	var cadastro CadastroCJ
+	if err := c.BindJSON(&cadastro); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	novaConta := criar_conta_cj(&cadastro)
+	c.JSON(http.StatusCreated, gin.H{"message": "Conta conjunta criada com sucesso", "conta": novaConta})
+}
+
+func checaExistencia(chave string) bool {
+	// Verifica se a chave existe em IndexcontasPF
+	_, existsPF := IndexcontasPF[chave]
+	// Verifica se a chave existe em IndexcontasPJ
+	_, existsPJ := IndexcontasPJ[chave]
+
+	// Retorna true se a chave existe em IndexcontasPF ou IndexcontasPJ
+	return existsPF || existsPJ
 }
 
 func main() {
@@ -194,7 +229,7 @@ func loginHandler(c *gin.Context) {
 		}
 	case 2:
 		if conta, exists := TContasPJ[req.NumConta]; exists {
-			if conta.Senha == req.Senha {
+			if conta.Senha == req.Senha && conta.CNPJ == req.CPFRAZAO {
 				c.JSON(http.StatusOK, gin.H{"message": "Login bem-sucedido"})
 				return
 			}
